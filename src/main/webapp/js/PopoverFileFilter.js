7c11424d1599a62e164f1897246d39c56cd62ad5
Greg Daneault <gregdaneault@gmail.com>
2014-11-21 20:09:23 -0500

    Cleanup filter

--- src/main/webapp/js/PopoverFileFilter.js
+++ src/main/webapp/js/PopoverFileFilter.js
@@ -1,24 +1,40 @@
-gyfp.filter('popoverFileList', ['$sce', function($sce) {
+gyfp.filter('popoverFileList', function() {
 
     var maxFiles = 10;
     return function(input) {
-        if (input.hasOwnProperty("length") && input.length == 0) {
+        var ret = "";
+
+        // Check that we're dealing with an array
+        if (!input.hasOwnProperty("length")) {
+            console.warn("Expected an array, got ", input);
+            return ret;
+        }
+
+        // Handle empty arrays
+        if (input.length == 0) {
             return "No files";
-        } else {
-            var ret = "";
+        }
+
+        // Build the real list
+        else {
             input.forEach(function(elem, i) {
+
+                // Limit the number of files to show to prevent the
+                // popover from getting too crazy
                 if (i < maxFiles) {
                     ret += elem.fileName + ", ";
                 }
             });
 
+            // Remove the trailing ', ' or add a '...' if there are
+            // more files remaining
             if (input.length < 10) {
                 ret = ret.substr(0, ret.length - 2);
             } else {
                 ret += "...";
             }
 
-            return $sce.trustAsHtml(ret);
+            return ret;
         }
     };
-}]);
+});
