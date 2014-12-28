3a63a2a7816f39c1414e70fe3e9f4fcbc9137f0f
Greg Daneault <gregdaneault@gmail.com>
2014-12-28 14:21:19 -0500

    Commenting TransferRequestController.js

--- src/main/webapp/js/controllers/TransferRequestController.js
+++ src/main/webapp/js/controllers/TransferRequestController.js
@@ -1,22 +1,46 @@
 gyfp.controller("TransferRequestController", ["$scope", "$modal", "$routeParams", "AuthenticationService", function($scope, $modal, $routeParams, authService) {
+
+
+    /**
+     * The request object to display
+     *
+     * @type {{id: string, files: array, requester: object, target: object}}
+     */
     $scope.request = {
-        id: $routeParams.requestId
+        id: $routeParams.requestId,
+        asdf: "asdf"
     };
+
 
     $scope.loaded = false;
     $scope.authenticated = false;
 
+    // Watch for change in authentication state
     $scope.$watch(authService.isAuthenticated, function(isAuthenticated) {
         $scope.authenticated = isAuthenticated;
         if (isAuthenticated) {
             $scope.loadRequest();
+        } else {
+            // If we are not authenticated, attempt to authenticate using
+            // existing credentials to prevent having to click the login button
+            authService.checkAuth();
         }
     });
 
+    /**
+     * Runs the load request for the transfer request
+     */
     $scope.loadRequest = function() {
-        gapi.client.gyfp.user.request.get({request: $scope.request.id}).execute($scope.applyRequest);
+        gapi.client.gyfp.user.request
+            .get({request: $scope.request.id})
+            .execute($scope.applyRequest);
     };
 
+    /**
+     * Processes a request response from loadRequest
+     *
+     * @param request  the request object returned from the api call.
+     */
     $scope.applyRequest = function(request) {
         $scope.loaded = true;
         if (request && request.error) {
@@ -28,6 +52,8 @@ gyfp.controller("TransferRequestController", ["$scope", "$modal", "$routeParams"
             $scope.request.target = request.targetUser;
         }
 
+        // This is not executed in the normal context of an angular call, so
+        // we need to manually digest
         $scope.$apply();
     }
 }]);
