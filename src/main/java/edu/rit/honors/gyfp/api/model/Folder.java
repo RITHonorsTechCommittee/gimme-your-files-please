fdaf5f11fbf195c7e6675d26f4bc59cd03450c57
Greg Daneault <gregdaneault@gmail.com>
2015-03-16 11:42:34 -0400

    Commenting and cleanup

--- src/main/java/edu/rit/honors/gyfp/api/model/Folder.java
+++ src/main/java/edu/rit/honors/gyfp/api/model/Folder.java
@@ -13,6 +13,7 @@ import com.google.api.services.drive.model.Permission;
 import com.google.appengine.api.users.User;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Ignore;
 import com.googlecode.objectify.annotation.Unindex;
 import edu.rit.honors.gyfp.api.Constants;
 import edu.rit.honors.gyfp.util.OfyService;
@@ -56,12 +57,14 @@ public class Folder {
     @Unindex
     private long updateTime;
 
+    @Ignore
     private ArrayList<AsyncFileList> currentFileLists;
 
     /**
      * Needed for Objectify
      */
-    private Folder() { }
+    private Folder() {
+    }
 
     /**
      * Creates a new folder object.
@@ -81,10 +84,15 @@ public class Folder {
      * This method first attempts to load a cached version of the folder data, and will only create a new Folder object
      * if the data does not yet exist or the cache is invalid
      *
-     * @param fileid The google fileid of the folder to load
-     * @param user   A user who is authenticated to google drive
+     * @param fileid
+     *         The google fileid of the folder to load
+     * @param user
+     *         A user who is authenticated to google drive
+     *
      * @return The corresponding folder
-     * @throws ForbiddenException If the drive service cannot be created
+     *
+     * @throws ForbiddenException
+     *         If the drive service cannot be created
      */
     public static Folder fromGoogleId(String fileid, User user) throws ForbiddenException {
         return fromGoogleId(fileid, user, false);
@@ -96,12 +104,17 @@ public class Folder {
      * This method first attempts to load a cached version of the folder data, and will only create a new Folder object
      * if the data does not yet exist or the cache is invalid
      *
-     * @param fileid       The google fileid of the folder to load
-     * @param user         A user who is authenticated to google drive
-     * @param forceRefresh Whether the cached version of the folder should be ignored and the entire folder
-     *                     recalculated
+     * @param fileid
+     *         The google fileid of the folder to load
+     * @param user
+     *         A user who is authenticated to google drive
+     * @param forceRefresh
+     *         Whether the cached version of the folder should be ignored and the entire folder recalculated
+     *
      * @return The corresponding folder
-     * @throws ForbiddenException If the drive service cannot be created
+     *
+     * @throws ForbiddenException
+     *         If the drive service cannot be created
      */
     public static Folder fromGoogleId(String fileid, User user, boolean forceRefresh) throws ForbiddenException {
 
@@ -133,8 +146,11 @@ public class Folder {
      * A helper method which can look up all the children of a specific file ID. This is the generic utility version of
      * the loadChildren method. This version can retrieve the children of any id, not just the for the current folder.
      *
-     * @param service An authenticated instance of the google drive service
-     * @param id      The id of the parent folder.
+     * @param service
+     *         An authenticated instance of the google drive service
+     * @param id
+     *         The id of the parent folder.
+     *
      * @return A list of files that have {@code id} as a parent
      */
     private static List<File> getChildren(Drive service, String id) {
@@ -230,28 +246,37 @@ public class Folder {
     }
 
     /**
-     * Loads the folder using batchs to minimize the number of required requests
+     * Loads the folder using batches to minimize the number of required requests
      *
-     * @param service The drive service
-     * @param id
-     * @throws IOException
+     * @param service
+     *         The drive service
      */
-    private void loadFileBatch(Drive service, String id) throws IOException {
-        final Files.List request = service.files().list();
+    private void loadChildren(Drive service) {
+        try {
+            final Files.List request = service.files().list();
 
-        request.setQ(String.format("'%s' in parents", id));
-        request.setFields(Constants.Field.FOLDER_ENUM);
+            request.setQ(String.format("'%s' in parents", id));
+            request.setFields(Constants.Field.FOLDER_ENUM);
 
-        FileList fileList = request.execute();
-        receiveFileList(new AsyncFileList(fileList, request, id));
+            FileList fileList = request.execute();
+            receiveFileList(new AsyncFileList(fileList, request, id));
 
-        loadFileBatchHelper(service);
+            loadFileBatchHelper(service);
+        } catch (IOException e) {
+            log.log(Level.SEVERE, "Error loading folder", e);
+        }
     }
 
+    /**
+     * The recursive helper method that processes all AFLs and enqueues all the new requests into a batch.
+     *
+     * @param service
+     *         The authenticated drive service
+     */
     private void loadFileBatchHelper(Drive service) {
-
         AsyncFileList afl = getNextList();
         BatchRequest batch = service.batch();
+
         while (afl != null && batch.size() < 100) {
             for (final File f : afl.list.getItems()) {
 
@@ -288,6 +313,8 @@ public class Folder {
                 }
             }
 
+            // Handle the paging.  Some requests could be split across multiple pages, and this sort of recursion can be
+            // treated exactly the same
             final Files.List request = afl.request;
             final String id = afl.id + " paged";
             // Also queue up the next page
@@ -315,37 +342,16 @@ public class Folder {
             afl = getNextList();
         }
 
+        // Actually execute the batch
         if (batch.size() > 0) {
             try {
                 log.info("Executing batch request with " + batch.size() + " requests.");
                 batch.execute();
                 loadFileBatchHelper(service);
-            }
-            catch (IOException e) {
+            } catch (IOException e) {
                 log.log(Level.SEVERE, "Error running batch", e);
             }
         }
-    }
-
-    /**
-     * Recursively loads the children of this folder and stores the children's owners and permissions
-     *
-     * @param service An authenticated instance of the google drive service
-     */
-    private void loadChildren(Drive service) {
-        try {
-            loadFileBatch(service, id);
-        } catch (IOException e) {
-            log.log(Level.SEVERE, "Error loading children", e);
-        }
-        /*
-        List<File> children = getChildren(service, id);
-        for (File f : children) {
-            TransferableFile tf = new TransferableFile(f);
-            for (Permission permission : f.getPermissions()) {
-                this.loadPermission(tf, permission);
-            }
-        }*/
     }
 
     /**
@@ -353,13 +359,18 @@ public class Folder {
      * <p/>
      * If the map already exists in the files map, it is returned. Otherwise a new entry is added to the map lookup
      *
-     * @param permission The permission that will be looked up
+     * @param permission
+     *         The permission that will be looked up
+     *
      * @return The populated map of permissions for this user
      */
     private FileUser getUser(Permission permission) {
         FileUser user = files.get(permission.getId());
 
-        if (user == null ) {
+        if (user == null) {
+
+            // Handle the two special cases:  "anyone" and "anyoneWithLink".  These users cannot own files so we can
+            // safely create "dummy" FileUsers
             if (Constants.Role.ANYONE.equals(permission.getId())) {
                 user = new FileUser(permission.getId(), permission.getId(), permission.getId());
             } else if (Constants.Role.ANYONE_WITH_LINK.equals(permission.getId())) {
@@ -377,8 +388,10 @@ public class Folder {
     /**
      * Saves a permission for a specific user and file.
      *
-     * @param file       The file which to which the user has access
-     * @param permission The actual permission of the user
+     * @param file
+     *         The file which to which the user has access
+     * @param permission
+     *         The actual permission of the user
      */
     private void loadPermission(TransferableFile file, Permission permission) {
         getUser(permission).addFile(permission.getRole(), file);
@@ -387,7 +400,9 @@ public class Folder {
     /**
      * Looks up a user and their files within this folder
      *
-     * @param userId The id of the user
+     * @param userId
+     *         The id of the user
+     *
      * @return The FileUser stored for this folder, or null if none exists
      */
     public FileUser getUser(String userId) {
@@ -397,11 +412,33 @@ public class Folder {
         return null;
     }
 
+    /**
+     * Helper data storage class.
+     * <p/>
+     * This facilitates the recursive loading of the folder.
+     * <p/>
+     * <ol> <li>Load the base folder in a single request.  This cannot be made more efficient via a batch, as there is
+     * only one api call to be made at this level.  The result of this is added to currentFileLists</li> <li>The
+     * recursive helper method then takes each AFL from the list currentFileLists.  Each file is added to the folder. If
+     * the file is a folder, a ew request is created and added to the batch.  The callback of this api call is then
+     * added to currentFileLists</li> <li>After processing all the files from the AFL, there is a check for paging.  If
+     * there is a next page token present, the execution of the next page is also added to the batch</li> </ol>
+     */
     private class AsyncFileList {
         private final FileList list;
         private final Files.List request;
         private String id;
 
+        /**
+         * Constructor.
+         *
+         * @param list
+         *         The file list result of the request
+         * @param request
+         *         The actual request that caused this execution
+         * @param id
+         *         The ID of the folder being loaded
+         */
         public AsyncFileList(FileList list, Files.List request, String id) {
             this.list = checkNotNull(list);
             this.request = checkNotNull(request);
