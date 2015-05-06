c2bc35bcbce28f34d0ac68135bad0e41b31b0744
regdoug <pierce.reginald@gmail.com>
2015-05-06 19:37:09 -0400

    Created about page

--- src/main/webapp/js/controllers/AboutController.js
+++ src/main/webapp/js/controllers/AboutController.js
@@ -1,3 +1,4 @@
 gyfp.controller('AboutController', ['$scope', function($scope) {
     $scope.appName = "Gimme Your Files, Please";
+    $scope.supportEmail = "technology at honors dot rit dot edu";
 }]);
