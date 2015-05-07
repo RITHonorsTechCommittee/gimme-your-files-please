c3d8cb2e325a0ca973ff85f80f3cb8ea011f332f
regdoug <pierce.reginald@gmail.com>
2015-05-07 19:26:03 -0400

    Updated about page
    - Removed commas from `appName` because it makes it hard to read
    - Changed transfer button color to correspond to new manage page color
    - Made example buttons smaller
    - Rephrased FAQ.

--- src/main/webapp/js/controllers/AboutController.js
+++ src/main/webapp/js/controllers/AboutController.js
@@ -1,4 +1,4 @@
 gyfp.controller('AboutController', ['$scope', function($scope) {
-    $scope.appName = "Gimme Your Files, Please";
-    $scope.supportEmail = "technology at honors dot rit dot edu";
+    $scope.appName = "Gimme Your Files Please";
+    $scope.supportEmail = "technology@honors.rit.edu";
 }]);
